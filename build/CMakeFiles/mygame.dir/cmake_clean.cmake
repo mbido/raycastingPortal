3abf65bee2b9ac2442d3152cbc51cf2026768bcd
mbido <ma.bidault39@gmail.com>
2023-09-20 14:00:57 +0200

    importation de gf réussie chez moi

---
+++ build/CMakeFiles/mygame.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mygame.dir/main.cpp.o"
+  "CMakeFiles/mygame.dir/main.cpp.o.d"
+  "mygame"
+  "mygame.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mygame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
