5161d33d422823581b08a92a6ff2a40688885955
Vital Focheux <115736317+vitalfocheux@users.noreply.github.com>
2023-10-18 18:19:37 +0200

    Modif MapTemplate.cpp pour la création de map en .png

--- sources/MapTemplate.cpp
+++ sources/MapTemplate.cpp
@@ -1,14 +1,22 @@
 #include "MapTemplate.hpp"
+#include <gf/Image.h>
 
 
 void MapTemplate::init() {
     // Initialize the map by setting all the values to 0 and the boundaries to 1
     for (int i = 0; i < m_nbRows; ++i) {
         for (int j = 0; j < m_nbColumns; ++j) {
+            
             if (i == 0 || j == 0 || i == m_nbRows - 1 || j == m_nbColumns - 1) {
                 m_map[i][j] = 1;
-            } else {
-                m_map[i][j] = 0;
+            }
+            else {
+                gf::Color4u pixelColor = m_image.getPixel({static_cast<unsigned>(i-1), static_cast<unsigned>(j-1)});
+                if(static_cast<int>(pixelColor.r) == 0 && static_cast<int>(pixelColor.g) == 0 && static_cast<int>(pixelColor.b) == 0){
+                    m_map[i][j] = 1;
+                }else{
+                    m_map[i][j] = 0;
+                }
             }
         }
     }
