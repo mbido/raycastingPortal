3057a8b7c96cbc39cc71d681c61409a1734f3d64
Vital Focheux <115736317+vitalfocheux@users.noreply.github.com>
2023-10-18 18:18:28 +0200

    Modif MapWalls.hpp pour la création de map en .png

--- sources/MapWalls.hpp
+++ sources/MapWalls.hpp
@@ -4,11 +4,12 @@
 #include "MapTemplate.hpp"
 
 #include <gf/RenderWindow.h>
+#include <gf/Image.h>
 
 class MapWalls : public MapTemplate {
     public :
-        MapWalls(int nbRows = 10, int nbColumns = 10)
-        : MapTemplate(nbRows, nbColumns)
+        MapWalls(std::string adress = "image.png"/*int nbRows = 10, int nbColumns = 10*/)
+        : MapTemplate(adress)
         {
         }
         void render(gf::RenderWindow& renderer, int sizeUnit);
