b5c61376201289a2774ec6fba354328af68f0335
vitalfocheux <vital.focheux@edu.univ-fcomte.fr>
2023-11-09 12:38:58 +0100

    Modification lors de l'initialisation pour la création des Murs

--- sources/MapTemplate.cpp
+++ sources/MapTemplate.cpp
@@ -1,24 +1,55 @@
 #include "MapTemplate.hpp"
 #include <gf/Image.h>
+#include <vector>
+#include <map>
+#include "Wall.hpp"
 
 
 void MapTemplate::init() {
     // Initialize the map by setting all the values to 0 and the boundaries to 1
+
+    std::map<int ,std::vector<std::pair<int,int>>> cellsWalls;
+    std::size_t nbWall = 0;
+
     for (int i = 0; i < m_nbRows; ++i) {
         for (int j = 0; j < m_nbColumns; ++j) {
             
             if (i == 0 || j == 0 || i == m_nbRows - 1 || j == m_nbColumns - 1) {
                 m_map[i][j] = 1;
+                cellsWalls[0].push_back(std::make_pair(i,j));
+                if(nbWall == 0){
+                    nbWall++;
+                }
+
             }
             else {
-                gf::Color4u pixelColor = m_image.getPixel({static_cast<unsigned>(i-1), static_cast<unsigned>(j-1)});
+                gf::Color4u pixelColor = m_image.getPixel({(i-1), (j-1)});
+                bool newWall = false;
                 if(static_cast<int>(pixelColor.r) == 0 && static_cast<int>(pixelColor.g) == 0 && static_cast<int>(pixelColor.b) == 0){
+                    for(std::size_t i = 0; i < nbWall+1; ++i){
+                        std::pair<int, int> cellToFindUp{i, j-1};
+                        std::pair<int, int> cellToFindRight{i-1, j};
+                        auto itUp = std::find(cellsWalls[i].begin(), cellsWalls[i].end(), cellToFindUp);
+                        auto itRight = std::find(cellsWalls[i].begin(), cellsWalls[i].end(), cellToFindRight);
+                        if(itUp != cellsWalls[i].end() || itRight != cellsWalls[i].end()){
+                            cellsWalls[i].push_back(std::make_pair(i, j));
+                            newWall = true;
+                            break;
+                        }
+                    }
+                    if(!newWall){
+                        nbWall++;
+                    }
                     m_map[i][j] = 1;
                 }else{
                     m_map[i][j] = 0;
                 }
             }
         }
+    }
+
+    for(auto cellWallsEntry : cellsWalls){
+        m_walls->push_back(Wall(cellWallsEntry.second));
     }
 }
 
