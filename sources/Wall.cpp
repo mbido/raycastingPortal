270688c00e838678a326796fbddb4b59d03b0641
vitalfocheux <vitalfocheux@gmail.com>
2023-11-20 15:45:04 +0100

    Initialisation des Walls avec seulement les sommets utiles

--- sources/Wall.cpp
+++ sources/Wall.cpp
@@ -1,15 +1,11 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
 
 #include "Wall.hpp"
 
 Wall::Wall(std::vector<std::pair<int, int>> occupiedCells) : wallCells(occupiedCells)
 {
-    head = new Vertex;
-    tail = new Vertex;
-    head->next = tail;
-    head->prev = tail;
-    tail->next = head;
-    tail->prev = head;
 
     // step 1: sorting the occupied cells by x and y (cf : notes)
     // step 2: creating a list of the useful vertices (cf : notes)
@@ -17,6 +13,83 @@ Wall::Wall(std::vector<std::pair<int, int>> occupiedCells) : wallCells(occupiedC
     //          a)-we start at the top left vertex of the first cell
     //          b)-we follow the next direction by priority : Right > Down > Left > Up
     //          c)-we repeat the step b) until we reach the top left vertex of the first cell again
+
+    int currentX = occupiedCells.front().first;
+    int currentY = occupiedCells.front().second;
+
+    vertices.push_back(std::make_pair(currentX, currentY));
+
+    std::size_t size = occupiedCells.size();
+    char dir = 'r';
+    currentX++;
+
+    for(std::size_t i = 1; i < size; ++i){
+        std::pair<int, int> currentVertex = {currentX, currentY};
+        std::pair<int, int> currentVertexUp = {currentX, currentY - 1};
+        std::pair<int, int> currentVertexUpLeft = {currentX - 1, currentY - 1};
+        std::pair<int, int> currentVertexLeft = {currentX - 1, currentY};
+
+        int j = 0;
+
+        if(std::find(occupiedCells.begin(), occupiedCells.end(), currentVertex) != occupiedCells.end()){
+            ++j;
+        }
+        if(std::find(occupiedCells.begin(), occupiedCells.end(), currentVertexUp) != occupiedCells.end()){
+            ++j;
+        }
+        if(std::find(occupiedCells.begin(), occupiedCells.end(), currentVertexUpLeft) != occupiedCells.end()){
+            ++j;
+        }
+        if(std::find(occupiedCells.begin(), occupiedCells.end(), currentVertexLeft) != occupiedCells.end()){
+            ++j;
+        }
+
+        if(j == 1 || j == 3){
+            vertices.push_back(currentVertex);
+            if(dir == 'r'){
+                if(j == 1){
+                    dir = 'd';
+                    currentY++;
+                }else if(j == 3){
+                    dir = 'u';
+                    currentY--;
+                }else{
+                    currentX++;
+                }
+            }else if(dir == 'd'){
+                if(j == 1){
+                    dir = 'l';
+                    currentX--;
+                }else if(j == 3){
+                    dir = 'r';
+                    currentX++;
+                }else{
+                    currentY++;
+                }
+            }else if(dir == 'l'){
+                if(j == 1){
+                    dir = 'd';
+                    currentY++;
+                }else if(j == 3){
+                    dir = 'u';
+                    currentY--;
+                }else{
+                    currentX--;
+                }
+            }else if(dir == 'u'){
+                if(j == 1){
+                    dir = 'r';
+                    currentX++;
+                }else if(j == 3){
+                    dir = 'l';
+                    currentX--;
+                }else{
+                    currentY--;
+                }
+            }
+        }
+    }
+
 }
 
 Wall::~Wall()
