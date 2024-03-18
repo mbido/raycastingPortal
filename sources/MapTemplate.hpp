8a09e1f487755bc8d5e6a221671f36accc19e3b9
mbido <ma.bidault39@gmail.com>
2024-03-18 09:46:40 +0100

    ajout de mes parties dans la présentation

--- sources/MapTemplate.hpp
+++ sources/MapTemplate.hpp
@@ -9,77 +9,82 @@
 #include <unordered_set>
 #include "Wall.hpp"
 
-class MapTemplate {
-    public :
-        MapTemplate(std::string adress = "../sources/map/image.png")
-        :m_image(gf::Image(adress))
-        ,m_nbRows(m_image.getSize().width)
-        ,m_nbColumns(m_image.getSize().height)
-        ,m_map(new int*[m_nbRows])
-        ,m_walls({})
+class MapTemplate
+{
+public:
+    MapTemplate(std::string adress = "../sources/map/image9.png")
+        : m_image(gf::Image(adress)), m_nbRows(m_image.getSize().width), m_nbColumns(m_image.getSize().height), m_map(new int *[m_nbRows]), m_walls({})
+    {
+        for (int i = 0; i < m_nbRows; ++i)
         {
-            for (int i = 0; i < m_nbRows; ++i) {
-                m_map[i] = new int[m_nbColumns];
-            }
-            init();
+            m_map[i] = new int[m_nbColumns];
         }
-        int setTile(int i, int j, int value);
-        
-        ~MapTemplate() {
-            for (int i = 0; i < m_nbRows; ++i) {
-                delete[] m_map[i];
-            }
-            delete[] m_map;
-        }
+        init();
+    }
+    int setTile(int i, int j, int value);
 
-        int getNbRows() const {
-            return m_nbRows;
+    ~MapTemplate()
+    {
+        for (int i = 0; i < m_nbRows; ++i)
+        {
+            delete[] m_map[i];
         }
+        delete[] m_map;
+    }
 
-        int getNbColumns() const {
-            return m_nbColumns;
-        }
+    int getNbRows() const
+    {
+        return m_nbRows;
+    }
 
-        int getTile(int i, int j) const {
-        if(i >= 0 && i < m_nbColumns && j >= 0 && j < m_nbRows)
+    int getNbColumns() const
+    {
+        return m_nbColumns;
+    }
+
+    int getTile(int i, int j) const
+    {
+        if (i >= 0 && i < m_nbColumns && j >= 0 && j < m_nbRows)
             return m_map[i][j];
         return -1;
-        }
+    }
 
-        std::vector<Wall> getWalls() const {
-            return m_walls;
-        }
+    std::vector<Wall> getWalls() const
+    {
+        return m_walls;
+    }
 
-        gf::Vector2f getBeginCell() const {
-            return m_begin;
-        }
+    gf::Vector2f getBeginCell() const
+    {
+        return m_begin;
+    }
 
-        gf::Vector2f getArrivalCell() const {
-            return m_arrival;
-        }
+    gf::Vector2f getArrivalCell() const
+    {
+        return m_arrival;
+    }
 
-        void depthFirstSearch(gf::Vector2f vertex, std::vector<gf::Vector2f>& visited, std::queue<gf::Vector2f> queue);
+    void depthFirstSearch(gf::Vector2f vertex, std::vector<gf::Vector2f> &visited, std::queue<gf::Vector2f> queue);
 
-        bool getSegments(gf::Vector2f point, std::vector<std::pair<gf::Vector2f, gf::Vector2f>> &segments);
+    bool getSegments(gf::Vector2f point, std::vector<std::pair<gf::Vector2f, gf::Vector2f>> &segments);
 
-    protected :
-        gf::Image m_image;
-        int m_nbRows;
-        int m_nbColumns;
-        int **m_map;
-        std::vector<Wall> m_walls;
-        gf::Vector2f m_begin;
-        gf::Vector2f m_arrival;
+protected:
+    gf::Image m_image;
+    int m_nbRows;
+    int m_nbColumns;
+    int **m_map;
+    std::vector<Wall> m_walls;
+    gf::Vector2f m_begin;
+    gf::Vector2f m_arrival;
 
-    
-    private :
-        /**
-         * @brief Initialize the map by setting all the values to 0 and the boundaries to 1
-        */
-        void init();
+private:
+    /**
+     * @brief Initialize the map by setting all the values to 0 and the boundaries to 1
+     */
+    void init();
 
-        void createWall(std::vector<gf::Vector2f> occupiedCells);
-        void setSortedVertices(const gf::Vector2f from, std::vector<gf::Vector2f> usefulVertices, std::vector<gf::Vector2f>& sortedVertices);
+    void createWall(std::vector<gf::Vector2f> occupiedCells);
+    void setSortedVertices(const gf::Vector2f from, std::vector<gf::Vector2f> usefulVertices, std::vector<gf::Vector2f> &sortedVertices);
 };
 
 #endif
--- sources/MapWalls.hpp
+++ sources/MapWalls.hpp
@@ -6,17 +6,18 @@
 #include <gf/RenderWindow.h>
 #include <gf/Image.h>
 
-class MapWalls : public MapTemplate {
-    public :
-        MapWalls(std::string address = "../sources/map/image4.png")
+class MapWalls : public MapTemplate
+{
+public:
+    MapWalls(std::string address = "../sources/map/image8.png")
         : MapTemplate(address)
-        {
-        }
-        void render(gf::RenderWindow& renderer, int sizeUnit);
-        std::vector<gf::Vector2f> getVertices();
+    {
+    }
+    void render(gf::RenderWindow &renderer, int sizeUnit);
+    std::vector<gf::Vector2f> getVertices();
 
-    private :
-        gf::Image m_image;
+private:
+    gf::Image m_image;
 };
 
 #endif
